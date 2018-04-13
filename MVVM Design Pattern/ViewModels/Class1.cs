99e34b25eda35b22392074a5b0a6495ef0159833
Alexander James Bochel <alexander.bochel@excella.com>
2018-04-13 11:59:31 -0400

    ViewModel: Added Traithlete ViewModel.

---
+++ MVVM Design Pattern/ViewModels/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM_Design_Pattern.Models;
+
+namespace MVVM_Design_Pattern.ViewModels
+{
+	public class TriathleteViewModel
+	{
+		private Triathlete _triathlete;
+
+		public TriathleteViewModel()
+		{
+			_triathlete = new Triathlete("Alex Bochel", 12);
+		}
+
+		public Triathlete Triathlete
+		{
+			get
+			{
+				return _triathlete;
+			}
+			set
+			{
+				_triathlete = value;
+			}
+		}
+	}
+}
